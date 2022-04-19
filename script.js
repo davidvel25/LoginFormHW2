38ea2c62580465570fe30c7bd551eb4f23b7106b
davidvel25 <davidvel25@outlook.com>
2022-04-18 20:53:31 -0700

    added simple css

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("select-login").disabled = false;
 document.getElementById("new-user").disabled = true;
 document.getElementById("existing-user").disabled = true;
+
+// document.getElementById("select-login").addEventListener("click", disable);
+// document.getElementById("new-user").addEventListener("click", disable);
+// document.getElementById("existing-user").addEventListener("click", disable);
