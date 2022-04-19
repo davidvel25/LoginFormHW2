e3bb48f2912df15cd637aed8ec83b7de282f10cc
davidvel25 <davidvel25@outlook.com>
2022-04-18 22:32:09 -0700

    finished the code (1 showing)
    only 1 field shows at a time

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-document.getElementById("select-login").disabled = false;
-document.getElementById("new-user").disabled = true;
-document.getElementById("existing-user").disabled = true;
+// document.getElementById("select-login").disabled = false;
+// document.getElementById("new-user").disabled = true;
+// document.getElementById("existing-user").disabled = true;
+
+function ClickerNewuser(){
+    document.getElementById("new-user").style.display = "block";
+    document.getElementById("select-login").disabled = true;
+    document.getElementById("new-user").disabled = false;
+}
+
+function ClickerExistinguser(){
+    document.getElementById("existing-user").style.display = "block";
+    document.getElementById("select-login").disabled = true;
+    document.getElementById("existing-user").disabled = false;
+}
+
+clickNewuser.addEventListener("click", ClickerNewuser);
+clickExistinguser.addEventListener("click", ClickerExistinguser);
+
+
+
+function SubmiterNewuser(){
+    alert("Welcome " + document.getElementById("uname1").value + "!");
+}
+
+function SubmiterExistinguser(){
+    alert("Welcome Back " + document.getElementById("uname2").value + "!");
+}
+
+submitNewuser.addEventListener("click", SubmiterNewuser);
+submitExistinguser.addEventListener("click", SubmiterExistinguser);
+
 
 // document.getElementById("select-login").addEventListener("click", disable);
 // document.getElementById("new-user").addEventListener("click", disable);
